13b2139bea4c3b84460108b7d38446f44afb4106
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-05-14 23:18:08 +0200

    Add the Test_Stem_Greek_Reference_File procedure and register it for execution

--- regtests/stemmer-tests.adb
+++ regtests/stemmer-tests.adb
@@ -58,6 +58,8 @@ package body Stemmer.Tests is
                        Test_Stem_German_Reference_File'Access);
       Caller.Add_Test (Suite, "Test Stemmer.Stem (Italian, Ref File)",
                        Test_Stem_Italian_Reference_File'Access);
+      Caller.Add_Test (Suite, "Test Stemmer.Stem (Greek, Ref File)",
+                       Test_Stem_Greek_Reference_File'Access);
    end Add_Tests;
 
    procedure Verify (T : in out Test;
@@ -219,5 +221,11 @@ package body Stemmer.Tests is
    begin
       Verify (T, L_ITALIAN, "regtests/files/it-test.txt");
    end Test_Stem_Italian_Reference_File;
+
+   --  Stem on Greek words using the reference file.
+   procedure Test_Stem_Greek_Reference_File (T : in out Test) is
+   begin
+      Verify (T, L_GREEK, "regtests/files/el-test.txt");
+   end Test_Stem_Greek_Reference_File;
 
 end Stemmer.Tests;
