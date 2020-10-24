e2ad2c32c4de48d39a84eac972ccdb493800f314
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-10-24 15:19:59 +0200

    Implement Test_Stem_Portuguese_Reference_File procedure and register for execution

--- regtests/stemmer-tests.adb
+++ regtests/stemmer-tests.adb
@@ -82,6 +82,8 @@ package body Stemmer.Tests is
                        Test_Stem_Nepali_Reference_File'Access);
       Caller.Add_Test (Suite, "Test Stemmer.Stem (Norwegian, Ref File)",
                        Test_Stem_Norwegian_Reference_File'Access);
+      Caller.Add_Test (Suite, "Test Stemmer.Stem (Portuguese, Ref File)",
+                       Test_Stem_Portuguese_Reference_File'Access);
    end Add_Tests;
 
    procedure Verify (T : in out Test;
@@ -315,5 +317,11 @@ package body Stemmer.Tests is
    begin
       Verify (T, L_NORWEGIAN, "regtests/files/no-test.txt");
    end Test_Stem_Norwegian_Reference_File;
+
+   --  Stem on Portuguese words using the reference file.
+   procedure Test_Stem_Portuguese_Reference_File (T : in out Test) is
+   begin
+      Verify (T, L_PORTUGUESE, "regtests/files/pt-test.txt");
+   end Test_Stem_Portuguese_Reference_File;
 
 end Stemmer.Tests;
